ad729cd2cf828d04704afbac1bce4833ec4dfaf2
LeoAttn <leo.massonnat@hotmail.fr>
2018-09-23 21:03:17 +0200

    compress old files

--- hdd_temp.py
+++ hdd_temp.py
@@ -14,14 +14,13 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-import os
-import argparse
-import datetime
-import re
-import csv
+import tarfile
+from argparse import ArgumentParser
 from collections import OrderedDict
-
+from csv import DictWriter, QUOTE_ALL, writer
+from datetime import datetime
+from os import path, popen, listdir
+from re import search, findall, MULTILINE, match
 
 # --------------- Main ----------------
 VERSION = "1.1"
@@ -31,13 +30,13 @@ regexTemp = r"^194 [\w-]+\s+0x\d+\s+\d+\s+\d+\s+\d+\s+[\w-]+\s+\w+\s+\S+\s+(\d+)
 regexMode = r"^(?:^Power mode is:\s+|^Device is in )(\w+)"
 
 # Parse arguments
-parser = argparse.ArgumentParser(description='Monitoring HDD and SDD temperatures')
+parser = ArgumentParser(description='Monitoring HDD and SDD temperatures')
 parser.add_argument('-p', '--path', help='Path of directory where the CSV file will be save', default='./')
 parser.add_argument('-d', '--devices', nargs='*', help='Device(s) to monitoring')
 parser.add_argument('-v', '--version', action='version', version='%(prog)s ' + VERSION)
 args = parser.parse_args()
 
-if not os.path.isdir(args.path):
+if not path.isdir(args.path):
     print("Directory is not valid")
     exit(5)
 
@@ -45,18 +44,18 @@ if args.devices:
     devices = args.devices
 else:
     # Find the list of all devices
-    cmdDevices = os.popen("/usr/sbin/smartctl -n standby --scan").read()
-    devices = re.findall(regexDevices, cmdDevices, re.MULTILINE)
+    cmdDevices = popen("/usr/sbin/smartctl -n standby --scan").read()
+    devices = findall(regexDevices, cmdDevices, MULTILINE)
 
 # Find the mode and the temperature for each device
 for device in devices:
-    date = datetime.datetime.now()
+    date = datetime.now()
 
-    cmdAttr = os.popen("/usr/sbin/smartctl -A " + device).read()
-    cmdInfo = os.popen("/usr/sbin/smartctl -n standby -i " + device).read()
+    cmdAttr = popen("/usr/sbin/smartctl -A " + device).read()
+    cmdInfo = popen("/usr/sbin/smartctl -n standby -i " + device).read()
 
-    temperature = re.search(regexTemp, cmdAttr, re.MULTILINE)
-    mode = re.search(regexMode, cmdInfo, re.MULTILINE)
+    temperature = search(regexTemp, cmdAttr, MULTILINE)
+    mode = search(regexMode, cmdInfo, MULTILINE)
 
     temperature = int(temperature.group(1)) if temperature else "Error"
     mode = mode.group(1) if mode else "Error"
@@ -64,23 +63,30 @@ for device in devices:
     # Define CSV path
     # dateFormated = date.strftime('%d-%m-%y')
 
-    csvFilename = 'hdd-temp_' + device.replace('/dev/', '') + '.csv'
+    csvFilename = 'hdd-temp_' + date.strftime('%b-%y') + '_' + device.replace('/dev/', '') + '.csv'
     csvPath = args.path + '/' + csvFilename
 
-    createHeader = not os.path.exists(csvPath)
+    fileNotExist = not path.exists(csvPath)
+
+    if fileNotExist:
+        allFiles = listdir(args.path)
+        for file in allFiles:
+            if match(r"^hdd-temp_\w+-\d{2}_" + device.replace('/dev/', '') + '.csv$', file):
+                print("true " + file)
+                with tarfile.open('hdd-temp_' + date.strftime('%b-%y') + '_' + device.replace('/dev/', ''), "w") as tar:
+                    tar.add(args.path + '/' + file)
 
     # Write the result in CSV file
     with open(csvPath, 'a') as csvFile:
         # Add a header if it's a new file
-        if createHeader:
+        if fileNotExist:
             header = OrderedDict([('Date', None), ('Power Mode', None), ('Temperature', None)])
-            dw = csv.DictWriter(csvFile, delimiter='\t', fieldnames=header)
+            dw = DictWriter(csvFile, delimiter='\t', fieldnames=header)
             dw.writeheader()
 
-        wr = csv.writer(csvFile, quoting=csv.QUOTE_ALL, delimiter=';')
+        wr = writer(csvFile, quoting=QUOTE_ALL, delimiter=';')
         wr.writerow([date, mode, temperature])
 
     print("Add row in " + csvPath)
-
 
 print("Monitor: " + str(len(devices)) + " devices.")
